a8a95dd68ce23ab388252a3fbaf856cecb1538e0
Vandesh Sawant <vandeshsawant@gmail.com>
2023-12-08 12:39:52 +0530

    Added dark mode

--- script.js
+++ script.js
@@ -58,3 +58,14 @@ formEl.addEventListener("submit", (event) => {
 showMore.addEventListener("click", () => {
   searchImage();
 });
+
+
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+    icon.src = "icons/sun.png";
+  } else {
+    icon.src = "icons/moon.png";
+  }
+}
